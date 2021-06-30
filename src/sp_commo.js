5f8834efe33529fddec857d90ec14eb9901c7397
roian <changwskr@naver.com>
2021-07-01 00:24:12 +0900

    web 555

--- src/sp_commo.js
+++ src/sp_commo.js
@@ -16,6 +16,8 @@ console.log("Server running at http://127.0.0.1:8080/");
 
 console.log("Server running at http://127.0.0.1:8080/");
 
+console.log("Server running at http://127.0.0.1:8080/");
+
 
 
 
