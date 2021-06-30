64345e57ef29dd205dbda924a46a6c23bd5e13b6
roian <changwskr@naver.com>
2021-07-01 00:08:15 +0900

    webhook home2

--- src/sp_commo.js
+++ src/sp_commo.js
@@ -13,8 +13,7 @@ var www = http.createServer(handler)
 www.listen(8080);
 
 console.log("Server running at http://127.0.0.1:8080/");
-console.log("Server running at http://127.0.0.1:8080/");
-console.log("Server running at http://127.0.0.1:8080/");
+
 
 
 
