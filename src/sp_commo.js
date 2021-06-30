ae1513d5c425f2ececfab667c4600c10150f1a79
changwskr <changwskr@gmail.com>
2021-06-30 19:08:07 +0900

    sp_commo.js webhook test

--- src/sp_commo.js
+++ src/sp_commo.js
@@ -13,10 +13,5 @@ var www = http.createServer(handler)
 www.listen(8080);
 
 console.log("Server running at http://127.0.0.1:8080/");
-console.log("Server running at http://127.0.0.1:8080/");
 
-console.log("Server running at http://127.0.0.1:8080/");
-console.log("Server running at http://127.0.0.1:8080/");
-
-console.log("Server running at http://127.0.0.1:8080/");
 
