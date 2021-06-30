112d97c7e2e553aae26b2f06d88e626545c6b1b1
changwskr <changwskr@gmail.com>
2021-06-30 14:15:16 +0900

    webhook 5 commit

--- src/sp_commo.js
+++ src/sp_commo.js
@@ -18,3 +18,5 @@ console.log("Server running at http://127.0.0.1:8080/");
 console.log("Server running at http://127.0.0.1:8080/");
 console.log("Server running at http://127.0.0.1:8080/");
 
+console.log("Server running at http://127.0.0.1:8080/");
+
